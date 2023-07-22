14b2ce297fece21fc45f9ac9c4b49a49b43ecbdf
George <66881900+bravepinata@users.noreply.github.com>
2023-07-22 16:03:16 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var memberContent = document.getElementById("memberContent");
+    var skillsContent = document.getElementById("skillsContent");
+    var projectsContent = document.getElementById("projectsContent");
+    var contactContent = document.getElementById("contactContent");
+    var aboutContent = document.getElementById("aboutContent");
+    member.style.color = "#000000";
+    skills.style.color = "#ffffff";
+    projects.style.color = "#ffffff";
+    contact.style.color = "#ffffff";
+    about.style.color = "#ffffff";
+    memberContent.style.display = "block";
+    skillsContent.style.display = "none";
+    projectsContent.style.display = "none";
+    contactContent.style.display = "none";
+    aboutContent.style.display = "none";
+}
